5cb73ca4f66c2b8e8b81de56a37e13f6f13951a4
Joe Chen <jpchen1@mainex1.asu.edu>
2018-08-30 23:13:36 -0700

    Added files

---
+++ mean.py
@@ -0,0 +1,5 @@
+
+
+
+def calc_mean(num_list):
+	return sum(num_list)/len(num_list)
---
+++ test_mean.py
@@ -0,0 +1,39 @@
+
+from mean import calc_mean
+
+# Test functions
+
+def test_int():
+	num_list = [1, 2, 3, 4, 5]
+
+	observed = calc_mean(num_list)
+	expected = 3
+
+	assert observed == expected
+
+
+def test_zero():
+	num_list = [0]
+
+	observed = calc_mean(num_list)
+	expected = 0
+
+	assert observed == expected
+
+
+def test_double():
+	num_list = [1, 2, 3, 4]
+
+	observed = calc_mean(num_list)
+	expected = 2.5
+
+	assert observed == expected
+
+
+def test_long():
+    big_number = 100000
+
+    observed = calc_mean(range(1,big_number))
+    expected = big_number/2.0
+
+    assert observed == expected
